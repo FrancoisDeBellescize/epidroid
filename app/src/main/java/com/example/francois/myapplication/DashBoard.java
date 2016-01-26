6c174b256ce55355fefb40825c4715c5d890f56e
naarsh <simon.leyendecker@epitech.eu>
2016-01-26 18:54:36 +0100

    history

--- app/src/main/java/com/example/francois/myapplication/DashBoard.java
+++ app/src/main/java/com/example/francois/myapplication/DashBoard.java
@@ -75,7 +75,7 @@ public class DashBoard extends FragmentActivity
 
 
 
-        
+
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
