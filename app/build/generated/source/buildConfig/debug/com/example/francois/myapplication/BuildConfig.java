c4676bd6ca057060333be96b0a1554eac08fc2d3
François <francois.de-bellescize@epitech.eu>
2016-01-26 14:25:21 +0100

    Attention ca va puer

---
+++ app/build/generated/source/buildConfig/debug/com/example/francois/myapplication/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.francois.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.francois.myapplication";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
