ca8417f7206c6ebb4a76474a8a59343644c39e74
François <francois.de-bellescize@epitech.eu>
2016-01-26 12:21:07 +0100

    Password ok !

--- app/src/main/java/com/example/francois/myapplication/MainActivity.java
+++ app/src/main/java/com/example/francois/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +16,9 @@ import com.google.gson.GsonBuilder;
 import com.loopj.android.http.*;
 
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import ConnectionObject.LoginObject;
 import cz.msebera.android.httpclient.Header;
@@ -32,19 +36,28 @@ public class MainActivity extends Activity {
 
         String login = settings.getString("login", null);
 
-        if (login != null){
+        if (login != null) {
             EditText edit_login = (EditText) findViewById(R.id.login);
             edit_login.setText(login);
             findViewById(R.id.password).requestFocus();
         }
     }
 
+    public String getStringFromEditText(EditText et) {
+
+        String ALLOWED_URI_CHARS = "<>@#&=*+-_.,:!?()/~'%";
+        String ret = null;
+        ret = Uri.encode(et.getText().toString(), ALLOWED_URI_CHARS);
+        return (ret);
+    }
+
     public void Connection(View view) {
 
-        EditText edit_login = (EditText) findViewById(R.id.login);
-        String login = edit_login.getText().toString();
-        EditText edit_password = (EditText) findViewById(R.id.password);
-        String password = edit_password.getText().toString();
+        String login = getStringFromEditText((EditText) findViewById(R.id.login));
+        String password = getStringFromEditText((EditText) findViewById(R.id.password));
+
+        Log.e("login : ", login);
+        Log.e("pwd : ", password);
 
         TextView error = (TextView) findViewById(R.id.error);
 
@@ -89,7 +102,7 @@ public class MainActivity extends Activity {
 
                 Gson gson = new GsonBuilder().create();
                 LoginObject tmp = gson.fromJson(errorResponse.toString(), LoginObject.class);
-                if (tmp.getError() != null){
+                if (tmp.getError() != null) {
                     TextView error = (TextView) findViewById(R.id.error);
                     error.setText(tmp.getError().getMessage());
                     Log.w("Error " + tmp.getError().getCode() + " : ", tmp.getError().getMessage());
